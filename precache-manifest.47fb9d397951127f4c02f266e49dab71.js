b9f5e7f21f442ac7740b7af1f76bb5aa7bebee96
Андрей Вахтёров <a.vakhterov@modulbank.ru>
2019-05-19 04:07:17 +0500

    Updates

---
+++ precache-manifest.47fb9d397951127f4c02f266e49dab71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24dfee3fc0d8c7e7689a7de4d8553b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9e1feb1486cdf05eb2",
+    "url": "/static/css/main.dd265daf.chunk.css"
+  },
+  {
+    "revision": "de68c95aa661b15156cd",
+    "url": "/static/js/2.5a1e2f27.chunk.js"
+  },
+  {
+    "revision": "af9e1feb1486cdf05eb2",
+    "url": "/static/js/main.b87df70e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
